42194d2fea7b0a7e949dc37b412faf895ac5ed72
jawisimo <ihorkolybaba@gmail.com>
2025-09-01 12:46:01 +0300

    fix(timezone): set JVM timezone to Europe/Kyiv for Ukraine (affects PostgreSQL connections)

--- src/main/java/com/jawisimo/urlshortener/UrlShortenerApiApplication.java
+++ src/main/java/com/jawisimo/urlshortener/UrlShortenerApiApplication.java
@@ -3,10 +3,13 @@ package com.jawisimo.urlshortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class UrlShortenerApiApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kyiv"));
         SpringApplication.run(UrlShortenerApiApplication.class, args);
     }
 
